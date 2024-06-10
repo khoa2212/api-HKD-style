f04a711d53689e2f551b8a33ac1fb146e34c2b59
Nguyen Tuan Khoa <nguyenkhoa438@gmail.com>
2024-06-10 10:59:14 +0700

    init product service

---
+++ product-service/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.6"
+	id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	val auth0JWTVersion = "4.4.0"
+
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.auth0:java-jwt:${auth0JWTVersion}")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
