dc13c678ecb62ff075da52dddd5b08692bce0d33
Nguyen Tuan Khoa <nguyenkhoa438@gmail.com>
2024-06-17 02:36:07 +0700

    not complete docker-compose

---
+++ product-service/mogo-seed/mongo-init.js
@@ -0,0 +1,321 @@
+// mongo-init.js
+db = db.getSiblingDB('HKD-style');
+
+// Create the 'products' collection and insert initial data
+db.createCollection('products');
+db.products.insertMany([
+   {
+      "_id": "390e0801-6b88-4301-9d49-a6bc26fd3ef0",
+      "name": "White Jeans",
+      "categories": ["MAN_CLOTHES", "SUMMER_CLOTHES"],
+      "description": "fusce congue diam  ornare imperdiet sapien urna pretium nisl ut volutpat sapien arcu sed augue aliquam",
+      "sales": 31,
+      "status": "ACTIVE",
+      "price": 140.25,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1718421898/products/gbrmv2c2jakwcbttlojz.png"
+      ]
+    },
+    {
+      "_id": "ae150a80-c790-43d5-b81b-dc24853a2ee6",
+      "name": "Cream Sleeveless T-Shirt",
+      "categories": ["WOMAN_CLOTHES", "BAS"],
+      "description": "vehicula consequat morbi a ipsum integer a nibh in quis",
+      "sales": 9,
+      "status": "ACTIVE",
+      "price": 106.98,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1717770330/products/wchbo34j2ienzuaaqr0i.png"
+      ]
+    },
+    {
+      "_id": "6f80406c-26e5-4d45-8b35-a8be47eed1bb",
+      "name": "Black Jeans",
+      "categories": ["KID_CLOTHES"],
+      "description": "orci nullam molestie nibh in lectus pellentesque at nulla suspendisse",
+      "sales": 0,
+      "status": "OUT_OF_STOCK",
+      "price": 185.36,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1718421895/products/ijp0ffievkup5lczfynr.png"
+      ]
+    },
+    {
+      "_id": "7cbec4a5-7932-4198-89c6-08636f1dedb2",
+      "name": "Blue T-Shirt",
+      "categories": ["SPORT_CLOTHES"],
+      "description": "nulla eget eros elementum pellentesque quisque porta volutpat erat quisque erat eros viverra eget congue eget semper rutrum",
+      "sales": 0,
+      "status": "ACTIVE",
+      "price": 101.79,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1717770330/products/bogmggtbjryon13jbbwq.png"
+      ]
+    },
+    {
+      "_id": "a35cae80-d512-41aa-ae30-a7ae8f15a749",
+      "name": "Black Bear T-Shirt",
+      "categories": ["WOMAN_CLOTHES", "BAS"],
+      "description": "id justo sit amet sapien dignissim vestibulum vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae",
+      "sales": 0,
+      "status": "ACTIVE",
+      "price": 108.6,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1718421895/products/fcuwir8uxjcemdhhbvdc.png"
+      ]
+    },
+    {
+      "_id": "29584d23-4b77-48c9-b6b5-20a793a064b9",
+      "name": "Black Angel T-Shirt",
+      "categories": ["MAN_CLOTHES", "SUMMER_CLOTHES"],
+      "description": "augue luctus tincidunt nulla mollis molestie lorem quisque ut erat curabitur gravida",
+      "sales": 67,
+      "status": "OUT_OF_STOCK",
+      "price": 109.6,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1717770329/products/z3h2o8cmwevr1ka7z7mx.png"
+      ]
+    },
+    {
+      "_id": "7312d3f4-74d1-49f7-a60b-2f888b67e677",
+      "name": "White What T-Shirt",
+      "categories": ["KID_CLOTHES"],
+      "description": "in hac habitasse platea dictumst aliquam augue quam sollicitudin vitae consectetuer eget rutrum at lorem integer tincidunt ante vel",
+      "sales": 0,
+      "status": "ACTIVE",
+      "price": 36.23,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1717770329/products/ru0nxikdwamwda0ehdea.png"
+      ]
+    },
+    {
+      "_id": "4ae360d1-adcd-4835-af3c-09889f059593",
+      "name": "White Angel T-Shirt",
+      "categories": ["SPORT_CLOTHES"],
+      "description": "luctus et ultrices posuere cubilia curae mauris viverra diam vitae quam suspendisse potenti nullam porttitor lacus at turpis donec posuere",
+      "sales": 33,
+      "status": "ACTIVE",
+      "price": 183.51,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1717770329/products/stws4wo1t4jzx0kef3kl.png"
+      ]
+    },
+    {
+      "_id": "fd0c2bc4-6651-4d05-8725-19dd0761aa6b",
+      "name": "Green T-Shirt",
+      "categories": ["WOMAN_CLOTHES", "BAS"],
+      "description": "leo maecenas pulvinar lobortis est phasellus sit amet erat nulla tempus vivamus in felis eu sapien",
+      "sales": 43,
+      "status": "OUT_OF_STOCK",
+      "price": 75.4,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1717770329/products/jwg1ilgpkevoml20cdnx.png"
+      ]
+    },
+    {
+      "_id": "7e98faa7-981c-4e37-b17c-739b6156a7a9",
+      "name": "Black Sleeveless T-Shirt",
+      "categories": ["MAN_CLOTHES", "SUMMER_CLOTHES"],
+      "description": "vestibulum sed magna at nunc commodo placerat praesent blandit nam nulla integer pede justo lacinia eget tincidunt",
+      "sales": 0,
+      "status": "ACTIVE",
+      "price": 154.88,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1717770329/products/q68b4strdnqczjio4tt4.png"
+      ]
+    },
+    {
+      "_id": "5173d085-acf5-4e11-bbcc-21c7d90d88e5",
+      "name": "Red Rose T-Shirt",
+      "categories": ["KID_CLOTHES"],
+      "description": "integer pede justo lacinia eget tincidunt eget tempus vel pede morbi porttitor lorem",
+      "sales": 68,
+      "status": "ACTIVE",
+      "price": 196.58,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1717770329/products/f3chxyhirztctbcxk2sf.png"
+      ]
+    },
+    {
+      "_id": "8e921fe0-1e3d-4f6d-897b-3670eef028f7",
+      "name": "Brown T-Shirt",
+      "categories": ["SPORT_CLOTHES"],
+      "description": "sapien cursus vestibulum proin eu mi nulla ac enim in",
+      "sales": 0,
+      "status": "OUT_OF_STOCK",
+      "price": 135.31,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1717770329/products/mwo8cru9mrg4z89s45dc.png"
+      ]
+    },
+    {
+      "_id": "19ff6e25-faa7-4e18-ac10-c2bddc55aebc",
+      "name": "XO T-Shirt",
+      "categories": ["WOMAN_CLOTHES", "BASIC_CLOTHES"],
+      "description": "et ultrices posuere cubilia curae mauris viverra diam vitae quam suspendisse potenti nullam porttitor",
+      "sales": 44,
+      "status": "ACTIVE",
+      "price": 196.32,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1718421895/products/iarrs77wc3libfpmugql.png"
+      ]
+    },
+    {
+      "_id": "c4d3aeeb-ad5d-4b62-8621-ac619fe5e209",
+      "name": "Dark Blue T-Shirt",
+      "categories": ["MAN_CLOTHES", "SUMMER_CLOTHES"],
+      "description": "vehicula consequat morbi a ipsum integer a nibh in quis justo maecenas rhoncus aliquam lacus morbi quis tortor id",
+      "sales": 52,
+      "status": "ACTIVE",
+      "price": 112.47,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1717770328/products/xrxww4ftsfbmfcqhgetv.png"
+      ]
+    },
+    {
+      "_id": "66d04f4d-3cfc-4dd4-a05d-4547bb6f2dbd",
+      "name": "Ligh Blue Sleeveless T-Shirt",
+      "categories": ["KID_CLOTHES"],
+      "description": "et ultrices posuere cubilia curae nulla dapibus dolor vel est donec odio justo sollicitudin ut suscipit a feugiat et eros",
+      "sales": 18,
+      "status": "OUT_OF_STOCK",
+      "price": 89.17,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1717770328/products/yr459wve4kmzo7utdvgk.png"
+      ]
+    },
+    {
+      "_id": "d4afea32-10c4-4bab-85e2-1ab5a077e8ab",
+      "name": "Light Cream T-Shirt",
+      "categories": ["SPORT_CLOTHES"],
+      "description": "hac habitasse platea dictumst maecenas ut massa quis augue luctus tincidunt nulla mollis molestie lorem",
+      "sales": 60,
+      "status": "ACTIVE",
+      "price": 103.46,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1717770329/products/g9njg44ydqtfka8guche.png"
+      ]
+    },
+    {
+      "_id": "afb1dd38-66f4-4920-ab47-d6407efc8a5b",
+      "name": "Dark Gray T-Shirt",
+      "categories": ["WOMAN_CLOTHES"],
+      "description": "nulla tempus vivamus in felis eu sapien cursus vestibulum proin eu mi nulla ac enim in tempor turpis",
+      "sales": 88,
+      "status": "ACTIVE",
+      "price": 50.5,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1717770328/products/x0rrehqqyo0bmge3haed.png"
+      ]
+    },
+    {
+      "_id": "91a53c15-312a-4bf1-9a44-a3ba5417578a",
+      "name": "Dark Cream T-Shirt",
+      "categories": ["MAN_CLOTHES"],
+      "description": "sapien arcu sed augue aliquam erat volutpat in congue etiam justo etiam pretium iaculis justo in hac habitasse platea dictumst",
+      "sales": 21,
+      "status": "OUT_OF_STOCK",
+      "price": 183.07,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1717770328/products/vccslhzlgxmgubh1utc1.png"
+      ]
+    },
+    {
+      "_id": "43d9c802-43f7-4095-be4e-b0ec2884a977",
+      "name": "Dark Cream Jeans",
+      "categories": ["KID_CLOTHES"],
+      "description": "sapien a libero nam dui proin leo odio porttitor id consequat in consequat ut nulla sed accumsan felis ut",
+      "sales": 64,
+      "status": "ACTIVE",
+      "price": 135.54,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1718421246/products/gppog3opneoxiofql0na.png"
+      ]
+    },
+    {
+      "_id": "a9e37017-9d94-455f-ac81-80d03191d4e1",
+      "name": "Black Shirt",
+      "categories": ["SPORT_CLOTHES"],
+      "description": "ac enim in tempor turpis nec euismod scelerisque quam turpis adipiscing lorem vitae mattis nibh ligula nec sem duis",
+      "sales": 58,
+      "status": "ACTIVE",
+      "price": 87.1,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1718421246/products/nife3htpyjuafsu4omgv.png"
+      ]
+    },
+    {
+      "_id": "3feb1c1a-804c-4060-8c70-f1ecc9f70fcf",
+      "name": "White Shirt",
+      "categories": ["WOMAN_CLOTHES"],
+      "description": "blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque",
+      "sales": 54,
+      "status": "OUT_OF_STOCK",
+      "price": 91.9,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1718421246/products/aqeyqzrhvremxdbqcsfj.png"
+      ]
+    },
+    {
+      "_id": "2b4caca6-b332-4769-91fa-45eb69ffb618",
+      "name": "Jeans Shirt",
+      "categories": ["MAN_CLOTHES"],
+      "description": "pede posuere nonummy integer non velit donec diam neque vestibulum eget vulputate ut ultrices vel augue vestibulum ante ipsum",
+      "sales": 11,
+      "status": "ACTIVE",
+      "price": 105.53,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1718421245/products/drgxe7vu3qfws1b0dumf.png"
+      ]
+    },
+    {
+      "_id": "eef27190-71ae-41d1-aa29-08cf6292eb09",
+      "name": "Dark Gray Hoodie",
+      "categories": ["KID_CLOTHES", "WINTER_CLOTHES"],
+      "description": "ac nulla sed vel enim sit amet nunc viverra dapibus nulla suscipit ligula in lacus curabitur at ipsum ac tellus",
+      "sales": 0,
+      "status": "ACTIVE",
+      "price": 116.43,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1718421245/products/kddpfrgtokcfsvkf8feb.png"
+      ]
+    },
+    {
+      "_id": "b4892ef8-f964-4542-a0b3-4306e9d6f111",
+      "name": "Light Gray Jeans",
+      "categories": ["SPORT_CLOTHES"],
+      "description": "sollicitudin ut suscipit a feugiat et eros vestibulum ac est lacinia nisi venenatis tristique fusce congue diam id",
+      "sales": 0,
+      "status": "OUT_OF_STOCK",
+      "price": 39.91,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1718421245/products/kimcbbv49zn1y6lhtw0n.png"
+      ]
+    },
+    {
+      "_id": "285dc8a0-2a98-40d8-a66d-6370ce6e14a4",
+      "name": "Dark Jeans Shirt",
+      "categories": ["WOMAN_CLOTHES"],
+      "description": "turpis nec euismod scelerisque quam turpis adipiscing lorem vitae mattis nibh ligula nec sem duis",
+      "sales": 0,
+      "status": "ACTIVE",
+      "price": 160.66,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1718421245/products/rvuuengshrokddngdusi.png"
+      ]
+    },
+    {
+      "_id": "89052b2a-f61d-444e-9ad5-c15cef1fc352",
+      "name": "Black XO Jeans",
+      "categories": ["MAN_CLOTHES"],
+      "description": "gravida sem praesent id massa id nisl venenatis lacinia aenean sit amet",
+      "sales": 0,
+      "status": "ACTIVE",
+      "price": 88.61,
+      "attachments": [
+        "https://res.cloudinary.com/dtzgqc50b/image/upload/v1718421245/products/ixidi5yne0pebfygoxx2.png"
+      ]
+    }
+]);
+
+print("Database and collection created, and data inserted successfully.");
